4eee4417b11f91c2c33e44cbda7813e546db77d1
delilah14 <dilluxdes@gmail.com>
2019-05-29 21:18:48 +0300

    Added

---
+++ ViewModels/NotepadViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VoiceDictation.ViewModels
+{
+    public class NotepadViewModel
+    {
+        public string boolCommand
+        {
+            get;
+            set;
+        }
+        public string underlineCommand
+        {
+            get; set;
+        }
+    }
+}
